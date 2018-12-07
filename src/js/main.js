9466fd7d5b5a481d6e73d5ad462258f2319ecd29
Brandon <BmcPrimary@yahoo.com>
2018-12-07 13:03:54 -0500

    Added pictures and languages used to project section. Made navbar responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import '../sass/main.scss';
+
+document.querySelectorAll('.nav__small__menu__list__item').forEach(
+    item => item.addEventListener('click', () => {
+        document.querySelector('.nav__small__checkbox').checked = false;
+    })
+);
